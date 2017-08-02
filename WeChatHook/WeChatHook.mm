716506a29c8609d7cf5e25d6e0254dc27e7783a5
yudu <du.yu@qunar.com>
2017-08-02 10:57:13 +0800

    Now U are on git

--- WeChatHook/WeChatHook.mm
+++ WeChatHook/WeChatHook.mm
@@ -10,9 +10,13 @@
 // see https://github.com/rpetrich/CaptainHook/
 
 #import <Foundation/Foundation.h>
+#import "Cycript/Cycript.h"
 #import "CaptainHook/CaptainHook.h"
+#import "FishConfigurationCenter.h"
 #include <notify.h> // not required; for examples only
 
+#define CYCRIPT_PORT 8888
+#define FishConfigurationCenterKey @"FishConfigurationCenterKey"
 // Objective-C runtime hooking using CaptainHook:
 //   1. declare class using CHDeclareClass()
 //   2. load class using CHLoadClass() or CHLoadLateClass() in CHConstructor
@@ -39,22 +43,80 @@
 @end
 
 
-@class ClassToHook;
+@class UIApplication;
+@class MicroMessengerAppDelegate;
 
-CHDeclareClass(ClassToHook); // declare class
+CHDeclareClass(UIApplication)
+CHDeclareClass(MicroMessengerAppDelegate)
+CHDeclareClass(CMessageMgr)
+CHDeclareClass(FindFriendEntryViewController)
+CHDeclareClass(MMTabBarController)
+CHDeclareClass(MMBadgeView)
+CHDeclareClass(WCDeviceStepObject)
+CHDeclareClass(NewMainFrameViewController)
+CHDeclareClass(UIView)
+CHDeclareClass(NewSettingViewController)
+CHDeclareClass(MMTableViewInfo)
+CHDeclareClass(MMTableViewSectionInfo)
+CHDeclareClass(MMTableViewCellInfo)
+CHDeclareClass(MMTableView)
+CHDeclareClass(UIViewController)
+CHDeclareClass(UILabel)
+CHDeclareClass(ChatRoomInfoViewController)
 
-CHOptimizedMethod(0, self, void, ClassToHook, messageName) // hook method (with no arguments and no return value)
-{
-	// write code here ...
-	
-	CHSuper(0, ClassToHook, messageName); // call old (original) method
+CHOptimizedMethod2(self, void, MicroMessengerAppDelegate, application, UIApplication *, application, didFinishLaunchingWithOptions, NSDictionary *, options){
+    CHSuper2(MicroMessengerAppDelegate, application, application, didFinishLaunchingWithOptions, options);
+    NSLog(@"## Start Cycript ##");
+    CYListenServer(CYCRIPT_PORT);
+    
+    NSLog(@"## Load FishConfigurationCenter ##");
+    NSData *centerData = [[NSUserDefaults standardUserDefaults] objectForKey:FishConfigurationCenterKey];
+    if (centerData) {
+        FishConfigurationCenter *center = [NSKeyedUnarchiver unarchiveObjectWithData:centerData];
+        [FishConfigurationCenter loadInstance:center];
+    }
+
 }
 
-CHOptimizedMethod(2, self, BOOL, ClassToHook, arg1, NSString*, value1, arg2, BOOL, value2) // hook method (with 2 arguments and a return value)
+CHDeclareMethod1(void, MicroMessengerAppDelegate, applicationWillResignActive, UIApplication *, application)
 {
-	// write code here ...
+    NSData *centerData = [NSKeyedArchiver archivedDataWithRootObject:[FishConfigurationCenter sharedInstance]];
+    [[NSUserDefaults standardUserDefaults] setObject:centerData forKey:FishConfigurationCenterKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
 
-	return CHSuper(2, ClassToHook, arg1, value1, arg2, value2); // call old (original) method and return its return value
+// 微信运动步数
+CHOptimizedMethod0(self, unsigned int, WCDeviceStepObject, m7StepCount)
+{
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    NSDateComponents *components = [cal components:(NSCalendarUnitEra | NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay) fromDate:[NSDate date]];
+    NSDate *today = [cal dateFromComponents:components];
+    components = [cal components:(NSCalendarUnitEra | NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay) fromDate:[FishConfigurationCenter sharedInstance].lastChangeStepCountDate];
+    NSDate *otherDate = [cal dateFromComponents:components];
+    BOOL modifyToday = NO;
+    if([today isEqualToDate:otherDate]) {
+        modifyToday = YES;
+    }
+    if ([FishConfigurationCenter sharedInstance].stepCount == 0 || !modifyToday) {
+        [FishConfigurationCenter sharedInstance].stepCount = CHSuper0(WCDeviceStepObject, m7StepCount);
+    }
+    return [FishConfigurationCenter sharedInstance].stepCount;
+}
+
+// 设置
+
+CHDeclareMethod0(void, NewSettingViewController, reloadTableData)
+{
+    CHSuper0(NewSettingViewController, reloadTableData);
+    MMTableViewInfo *tableInfo = [self valueForKeyPath:@"m_tableViewInfo"];
+    MMTableViewSectionInfo *sectionInfo = [objc_getClass("MMTableViewSectionInfo") sectionInfoDefaut];
+    MMTableViewCellInfo *nightCellInfo = [objc_getClass("MMTableViewCellInfo") switchCellForSel:@selector(handleNightMode:) target:[FishConfigurationCenter sharedInstance] title:@"夜间模式" on:[FishConfigurationCenter sharedInstance].isNightMode];
+    [sectionInfo addCell:nightCellInfo];
+    MMTableViewCellInfo *stepcountCellInfo = [objc_getClass("MMTableViewCellInfo") editorCellForSel:@selector(handleStepCount:) target:[FishConfigurationCenter sharedInstance] title:@"微信运动步数" margin:300.0 tip:@"请输入步数" focus:NO text:[NSString stringWithFormat:@"%ld", (long)[FishConfigurationCenter sharedInstance].stepCount]];
+    [sectionInfo addCell:stepcountCellInfo];
+    [tableInfo insertSection:sectionInfo At:0];
+    MMTableView *tableView = [tableInfo getTableView];
+    [tableView reloadData];
 }
 
 static void WillEnterForeground(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
@@ -65,6 +127,10 @@ static void WillEnterForeground(CFNotificationCenterRef center, void *observer, 
 static void ExternallyPostedNotification(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
 {
 	// not required; for example only
+}
+
+__attribute__((constructor)) static void entry() {
+    NSLog(@"Hello,WeChat!");
 }
 
 CHConstructor // code block that runs immediately upon load
@@ -82,8 +148,12 @@ CHConstructor // code block that runs immediately upon load
 		
 		// CHLoadClass(ClassToHook); // load class (that is "available now")
 		// CHLoadLateClass(ClassToHook);  // load class (that will be "available later")
-		
-		CHHook(0, ClassToHook, messageName); // register hook
-		CHHook(2, ClassToHook, arg1, arg2); // register hook
+		CHLoadLateClass(MicroMessengerAppDelegate);
+		CHHook2(MicroMessengerAppDelegate, application, didFinishLaunchingWithOptions); // register hook
+        CHLoadLateClass(WCDeviceStepObject);
+        CHHook0(WCDeviceStepObject, m7StepCount);
+
+
+        
 	}
 }
